9e844c1903f47bb4c01dd17e9ac108b268aeb39b
MthnYtr <metehanyeter1144@gmail.com>
2026-01-06 15:24:20 +0300

    Update game.cpp - UI location and Gold defined in the game
    UI location was determined and Gold defined in the game

--- game.cpp
+++ game.cpp
@@ -1,16 +1,24 @@
 #include "game.h"
+#include "raylib.h"
 
-Game::Game()
-{
-    // Henüz işlem yok
+Game::Game() {
+    gold = 500;       // Oyuncunun başlangıç para değeri
+    gameOver = false;
 }
 
 void Game::Update()
 {
-    // Henüz bir mantık yok
+ 
 }
 
 void Game::Draw()
 {
   
+    // Basit UI 
+    DrawText(" Game ", 20, 20, 20, DARKGRAY);
+    DrawText(TextFormat("Gold: %d", gold), 20, 50, 20, GOLD);
+
+    if (gameOver) {
+        DrawText("GAME OVER", 400, 300, 40, RED);
+    }
 }
