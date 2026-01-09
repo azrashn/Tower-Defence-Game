92c9128dac1d259726d93079dde6917598d74dfe
MthnYtr <metehanyeter1144@gmail.com>
2026-01-09 19:10:46 +0300

    Added mouse click system and time system for tile
    Added code for tile. Click button to know tile select system. And time system for tiles to be add

--- game.cpp
+++ game.cpp
@@ -16,6 +16,24 @@ void Game::Update()
 {
     map.Update();
 
+    Vector2 mousePosition = GetMousePosition(); // mouse un ekrandaki pozisyonu hesapla
+    mouseTile = map.CheckTile(mousePosition); // tile bul
+
+    // Mouse aynı tile üzerindeyse süre artsın
+    if (mouseTile && mouseTile == lastMouseTile)
+    {
+       mouseTime += GetFrameTime(); // Son çizilen frama ile önceki arasındaki süre belirlemek için 60FPS
+    }
+    else // diğer türlü tile süresini sıfırla
+    {
+      mouseTime = 0.0f;
+      lastMouseTile = mouseTile;
+    }
+
+    if (mouseTile && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) { // Click yapısı tile ile
+         selectedTile = mouseTile; // yapılan click anını adreste saklıyor. Amaç tıklandığı anda tile seçmek
+    }
+
 }
 
 void Game::Draw()
@@ -26,6 +44,10 @@ void Game::Draw()
     // Basit UI 
     DrawText(" Game ", 20, 20, 20, DARKGRAY);
     DrawText(TextFormat("Gold: %d", gold), 20, 50, 20, GOLD);
+
+    if (selectedTile) {
+    DrawRectangleLinesEx(selectedTile->rect, 3, BLUE); // seçili tile vurgulamak için
+    }
 
     if (gameOver) {
         DrawText("GAME OVER", 400, 300, 40, RED);
