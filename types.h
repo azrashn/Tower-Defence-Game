516dce0a1feace40af1c8465fba6210d57f75092
MthnYtr <metehanyeter1144@gmail.com>
2026-01-11 15:45:30 +0300

    some definitions changed
    types.h updated for changed some definitions.

--- types.h
+++ types.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "raylib.h"
 
-enum class TileState
+enum class TileType // state -> type dönüştürdüm. map.h kısmı buraya gelicek
 {
     Empty,
     Blocked,
@@ -11,7 +11,12 @@ enum class TileState
 struct Tile
 {
     Rectangle rect; // ekranın kapladığı alanı temsil ediyor
-    TileState state;
+    TileType type;
+ 
+    // Genel tanımlamalar. map.h kısmı
+    bool occupied;
+    Color color;
+    int id;
 
     // x ve y tile'ı yapıp harita kordinat için tanımlandı
     int gridX;
