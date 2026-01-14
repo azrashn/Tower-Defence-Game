1972ba9a2a228cb38d7490e9ba8c034bbbca6515
azrashn <esma.azra.sahin24@gmail.com>
2026-01-14 23:45:51 +0300

    Update enemy.h file

--- enemy.h
+++ enemy.h
@@ -1,31 +1,75 @@
-#pragma once
-#include "raylib.h"
-#include <vector>
+#include "enemy.h"
+#include "types.h"
+#include <cmath>
 
-enum EnemyType {
-    GOBLIN,
-    ORC
-};
+Enemy::Enemy(const std::vector<Vector2>& pathPoints)  {
+    type = GOBLIN; 
 
-class Enemy {
-public:
-    Enemy(EnemyType type);
+    position = path[0];
+    currentTarget = 1;
+    active = true;
 
-    void Update();
-    void Draw();
-    void TakeDamage(float amount);
+    if (type == GOBLIN) {
+        speed = 3.5f;
+        maxHealth = 40.0f;
+        radius = 8.0f;
+    }
+    else if (type == ORC) {
+        speed = 2.0f;
+        maxHealth = 90.0f;
+        radius = 12.0f;
+    }
 
-    bool active;
-    Vector2 position;
+    health = maxHealth;
+}
 
-private:
-    std::vector<Vector2> path;
-    int currentTarget;
+void Enemy::Update() {
+    if (!active || path.empty()) return;
 
-    EnemyType type;
+    if (currentTarget >= path.size()) {
+        active = false;
+        return;
+    }
 
-    float speed;
-    float health;
-    float maxHealth;
-    float radius;
-};
+    Vector2 target = path[currentTarget];
+
+    if (position.x < target.x) position.x += speed;
+    else if (position.x > target.x) position.x -= speed;
+
+    if (position.y < target.y) position.y += speed;
+    else if (position.y > target.y) position.y -= speed;
+
+    if (fabs(position.x - target.x) <= speed &&
+        fabs(position.y - target.y) <= speed) {
+        position = target;
+        currentTarget++;
+    }
+}
+
+void Enemy::TakeDamage(float amount) {
+    health -= amount;
+    if (health <= 0) {
+        health = 0;
+        active = false;
+    }
+}
+
+void Enemy::Draw() {
+    if (!active) return;
+
+    Color bodyColor = RED;
+    if (type == GOBLIN) bodyColor = LIME;
+    else if (type == ORC) bodyColor = DARKGREEN;
+
+    DrawCircleV(position, radius, bodyColor);
+    DrawCircleLines(position.x, position.y, radius, BLACK);
+
+    float hpRatio = health / maxHealth;
+    DrawRectangle(
+        position.x - 12,
+        position.y - 20,
+        24 * hpRatio,
+        4,
+        GREEN
+    );
+}
