f8744cbb64d37a33de0c1d85a999fd46d8424b49
MthnYtr <metehanyeter1144@gmail.com>
2026-01-16 16:59:47 +0300

    Added constants.h for dinamic screen and upgraded main
    Added constants.h for dinamic screen. Also start prepare to upgraded main.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "raylib.h"
 #include "game.h"
 
+enum GameState {
+    MENU,       // Ana Menü
+    GAMEPLAY,   // Oyun Oynanıyor
+    PAUSED,     //duraklama
+    GAME_OVER   // bitirme
+};
+
 int main()
-{
-    InitWindow(1400, 1000, "Castle Defense - Map Test");
+{    // dinamik ayarlama için
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Castle Defense - Map Test");
     SetTargetFPS(60);
 
     Game game;
-
+    GameState currentState = MENU;
+ 
     while (!WindowShouldClose())
     {
         game.Update();
